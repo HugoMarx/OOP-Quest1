272ac7d000123d9673a8a54d6166b8bcfb4904d0
Hugo Marx <marx.hugo@gmail.com>
2022-03-28 23:22:02 +0200

    OOP Quest 2

---
+++ Truck.php
@@ -0,0 +1,50 @@
+<?php
+
+require_once('Vehicule.php');
+
+class Truck extends Vehicule
+{
+    private string $energy;
+    private int $storageCapacity;
+    private int $payload = 0;
+   
+
+    public function __construct(string $color, int $nbSeats, string $energy, int $storageCapacity)
+    {
+        parent::__construct($color, $nbSeats);
+        $this->energy = $energy;
+        $this->storageCapacity = $storageCapacity;
+    }
+
+    public function getStorage(): int
+    {
+        return $this->storageCapacity;
+    }
+
+    public function setStorage(int $storageCapacity): void
+    {
+        $this->storageCapacity = $storageCapacity;
+    }
+
+    public function getPayload(): int
+    {
+        return $this->payload;
+    }
+
+    public function setPayload(int $payload)
+    {
+        $this->payload = $payload;
+    }
+
+
+    public function getFilling(): string
+    {
+        if ($this->payload < $this->storageCapacity) {
+            return 'in filling';
+        } elseif ($this->payload == $this->storageCapacity) {
+            return 'full';
+        } else {
+            return 'Payload too large';
+        }
+    }
+}
--- _index.php
+++ _index.php
@@ -1,6 +1,9 @@
 <?php
-require('Car.php');
-require('Bicycle.php');
+require_once('Car.php');
+require_once('Bicycle.php');
+require_once('Truck.php');
+
+echo '<h1> Car </h1>';
 
 $myCar = new Car('black', 5, 'diesel');
 
@@ -14,6 +17,7 @@ var_dump($myCar);
 
 var_dump($myCar->getColor());
 
+echo '<h1> Bike </h1>';
 
 $bike = new Bicycle('red', 5, 1);
 
@@ -22,3 +26,17 @@ echo $bike->forward();
 echo $bike->brake();
 
 var_dump($bike);
+
+echo '<h1> Truck </h1>';
+
+$myTruck = new Truck('green', 4, 'electric', 50);
+
+$myTruck->setPayload(50);
+
+echo $myTruck->forward();
+
+echo $myTruck->brake();
+
+echo '<h2>' . $myTruck->getFilling() . '</h2>';
+
+var_dump($myTruck);
